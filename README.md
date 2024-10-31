035ebe1a00b354739ec05617100928cf384f97b7
zhawjtran <95748099+zhawjtran@users.noreply.github.com>
2024-10-31 21:19:39 +0000

    save current work

--- README.md
+++ README.md
@@ -10,7 +10,15 @@ DBHOST=localhost
 DBUSER=app_user
 DBPASS=app_password
 
-2. Run the migrations:
+2. When you starting GitHub Codespace , it takes some time until all the postCreateCommand (which install the requirements.txt), in the tab terminal below, it inicates the current work. As soon its done, it will disapear and you can continue with the next step.
+
+3. Initial the migration folder
+    ```shell
+    python3 -m flask db init
+    ```
+
+
+3. Run the migrations:
 
     ```shell
     python3 -m flask db upgrade
--- app.py
+++ app.py
@@ -4,13 +4,27 @@ from datetime import datetime
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
 
 app = Flask(__name__, static_folder='static')
 
+load_dotenv()
+
+#app.config.update(
+#    SQLALCHEMY_DATABASE_URI=f"postgresql://{os.getenv('DBUSER')}:{os.getenv('DBPASS')}@{os.getenv('DBHOST')}/{os.getenv('DBNAME')}",
+#    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+#)
+
 app.config.update(
-    SQLALCHEMY_DATABASE_URI=app.config.get('DATABASE_URI'),
+    SQLALCHEMY_DATABASE_URI=f"postgresql://{os.getenv('DBUSER')}:{os.getenv('DBPASS')}@{os.getenv('DBHOST')}/{os.getenv('DBNAME')}",
     SQLALCHEMY_TRACK_MODIFICATIONS=False,
 )
+
+#app.config.update(
+#    SQLALCHEMY_DATABASE_URI=app.config.get('DATABASE_URI'),
+#    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+#)
 
 # Initialize the database connection
 db = SQLAlchemy(app)
